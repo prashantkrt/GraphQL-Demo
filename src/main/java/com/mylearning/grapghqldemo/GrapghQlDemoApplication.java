6c239c8b7a3346f2769e9645743ab1c458bcbd90
prashant <prashanttiwary10@gmail.com>
2025-03-18 14:45:17 +0530

    Initial Commit

---
+++ src/main/java/com/mylearning/grapghqldemo/GrapghQlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mylearning.grapghqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapghQlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrapghQlDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mylearning/grapghqldemo/GrapghQlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.grapghqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapghQlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
